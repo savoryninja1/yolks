a837850eb372ab272ac8f50b6a838dec3f182037
savoryninja1 <95149891+savoryninja1@users.noreply.github.com>
2024-02-02 00:05:09 -0800

    Update entrypoint.sh

--- wine/entrypoint.sh
+++ wine/entrypoint.sh
@@ -10,7 +10,7 @@ wine --version
 
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
 eval ${MODIFIED_STARTUP}
