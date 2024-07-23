0099d0941398d74d03b470245d8318119d9f5ffc
savoryninja1 <savoryninjagamer@gmail.com>
2024-07-23 01:47:15 -0700

    P-J added

---
+++ P-J/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd /home/container
+
+# Set environment variable that holds the Internal Docker IP
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+
+export DOTNET_ROOT=/usr/share/
+
+python -version
+java --version
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
